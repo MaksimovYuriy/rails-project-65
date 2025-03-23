cbb179c5d23e494a71d2873c4c4673e12a7ec5cc
Yura <yura.maksimov.04@mail.ru>
2025-03-23 14:18:58 +0300

    Добавлен Sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Sentry.init do |config|
+  config.dsn = 'https://584a10c5c2c506f15c3db76cd0092133@o4508844991053824.ingest.de.sentry.io/4509026784116816'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true
+end
