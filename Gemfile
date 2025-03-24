e472d816b8bc1839b1e3b4cb3f3ac5b6eb102260
Yura <yura.maksimov.04@mail.ru>
2025-03-24 21:36:51 +0300

    Готовая авторизация

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'omniauth-rails_csrf_protection'
 
 gem 'faker'
 
+gem 'simple_form'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.2.2", ">= 7.2.2.1"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -1,7 +1,7 @@
 module Web
     class AuthController < ApplicationController
         def callback
-            auth = request.env["omniauth.auth"]
+            auth = request.env['omniauth.auth']
             user = User.find_or_create_by(email: auth.info.email) do |u|
                 u.name = auth.info.name
             end
