b6e220b9f93f209cd7947054ea7cb99f9b06c3d5
Yura <yura.maksimov.04@mail.ru>
2025-03-26 11:41:21 +0300

    Кнопки логина/логаута вернулись на свои места (simple_form_for заменен на button_to)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   scope module: :web do
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
-    delete 'logout', to: 'auth#logout', as: :logout
+    delete 'logout', to: 'auth#logout', as: :auth_logout
   end
 
   resources :welcome
