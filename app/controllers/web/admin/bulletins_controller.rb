b3b0539b710c7876395b14e3ccb03d072d477334
Yura <yura.maksimov.04@mail.ru>
2025-04-07 17:52:03 +0300

    Интеграционные тесты

--- app/controllers/web/admin/bulletins_controller.rb
+++ app/controllers/web/admin/bulletins_controller.rb
@@ -4,46 +4,46 @@ module Web
             before_action :authenticate_user!
 
             def index
+                authorize Bulletin, policy_class: Web::Admin::BulletinPolicy
                 @search_query = Bulletin.ransack(params[:search_query])
                 @bulletins = @search_query.result
                     .order(created_at: :desc)
                     .page(params[:page])
-                @bulletins.each do |bulletin|
-                    authorize bulletin, policy_class: Web::Admin::BulletinPolicy
-                end
             end
 
             def on_moderate
+                authorize Bulletin, policy_class: Web::Admin::BulletinPolicy
                 @search_query = Bulletin.ransack(params[:search_query])
                 @bulletins = @search_query.result
                     .where(aasm_state: "under_moderation")
                     .order(created_at: :desc)
                     .page(params[:page])
-                @bulletins.each do |bulletin|
-                    authorize bulletin, policy_class: Web::Admin::BulletinPolicy
-                end
             end
 
             def to_moderate
                 @bulletin = Bulletin.find(params[:id])
+                authorize @bulletin, policy_class: Web::Admin::BulletinPolicy
                 @bulletin.to_moderate!
                 redirect_to admin_bulletins_path
             end
 
             def reject
                 @bulletin = Bulletin.find(params[:id])
+                authorize @bulletin, policy_class: Web::Admin::BulletinPolicy
                 @bulletin.reject!
                 redirect_to admin_bulletins_path
             end
 
             def publish
                 @bulletin = Bulletin.find(params[:id])
+                authorize @bulletin, policy_class: Web::Admin::BulletinPolicy
                 @bulletin.publish!
                 redirect_to admin_bulletins_path
             end
 
             def archive
                 @bulletin = Bulletin.find(params[:id])
+                authorize @bulletin, policy_class: Web::Admin::BulletinPolicy
                 @bulletin.archive!
                 redirect_to admin_bulletins_path
             end
--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -11,11 +11,66 @@ class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
     end
 
     test "index page" do
-        get root_url
-        assert_response :success
+      get root_url
+      assert_response :success
+    end
+
+    test "profile page" do
+      get profile_path
+      assert_redirected_to root_path
+
+      sign_in users(:user)
+      get profile_path
+      assert_response :success
+    end
+
+    test "new bulletin page" do
+      get new_bulletin_path
+      assert_redirected_to root_path
+
+      sign_in users(:user)
+      get new_bulletin_path
+      assert_response :success
+    end
+
+    test "new bulletin without image" do
+      sign_in users(:user)
+
+      attrs_without_image = { category_id: categories(:category2)[:id],
+                description: 'test description',
+                title: 'test title' }
+      
+      post bulletins_url, params: { bulletin: attrs_without_image }
+
+      bulletin = Bulletin.find_by attrs_without_image
+      assert { bulletin.nil? }    
+    end
+
+    test "update bulletin" do
+      sign_in users(:user)
+      updated_attrs = {
+        title: 'updated title'
+      }
+
+      patch bulletin_path(@bulletin), params: { bulletin: updated_attrs }
+      assert_redirected_to bulletin_path(@bulletin)
+
+      @bulletin.reload
+      assert_equal @bulletin.title, 'updated title'
     end
 
     test "bulletin states (admin == false)" do
-      debugger
+      sign_in users(:user)
+
+      patch to_moderate_bulletin_path(@bulletin), params: {}
+      assert_response :redirect
+      assert_equal 'under_moderation', @bulletin.reload.aasm_state
+
+      patch publish_admin_bulletin_path(@bulletin), params: {}
+      assert_equal 'under_moderation', @bulletin.reload.aasm_state
+
+      patch archive_bulletin_path(@bulletin), params: {}
+      assert_response :redirect
+      assert_equal 'archived', @bulletin.reload.aasm_state
     end
 end
