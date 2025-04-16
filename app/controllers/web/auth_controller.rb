911518190ed6f4ea159e0ba9e19071f8156589f8
Yura <yura.maksimov.04@mail.ru>
2025-04-16 16:11:16 +0300

    Убран :user_admin из сессии

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -4,23 +4,19 @@ module Web
   class AuthController < Web::ApplicationController
     def callback
       auth = request.env['omniauth.auth']
-      user = User.find_or_create_by(email: auth.info.email) do |u|
-        u.name = auth.info.name
-      end
+      user = User.find_or_create_by(email: auth.info.email)
+      user.update(name: auth.info.name)
       session[:user_id] = user.id
-      session[:user_admin] = user.admin?
       redirect_to root_path, notice: "#{I18n.t('notices.auth.logged')} #{user.name}"
     end
 
     def logout
       session[:user_id] = nil
-      session[:user_admin] = nil
       redirect_to root_path, notice: I18n.t('notices.auth.logout')
     end
 
     def failure
       session[:user_id] = nil
-      session[:user_admin] = nil
       redirect_to root_path, notice: I18n.t('notices.auth.failure')
     end
   end
